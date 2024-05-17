ab6046387a6f7e7a3d4ad6bd2141761c7faf9609
Aldrich <aldrich.adriel@gmail.com>
2024-05-17 15:51:02 -0700

    added the second chart

--- SecondChart.R
+++ SecondChart.R
@@ -1,2 +1,10 @@
 library(ggplot2)
 
+setwd("~/Documents/archive")
+data <- read.csv("test.csv")
+ggplot(data, mapping = aes(x = education, y = occupation, color = gender)) +
+  geom_point(position = "dodge") + 
+  labs(x = "Education", y = "Occupation", fill = "Gender", title = "Comparing 
+       education and occupation")
+
+
